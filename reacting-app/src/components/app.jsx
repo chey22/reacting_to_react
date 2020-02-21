fcd6431f323840ae5c07b6dca0bf81c3bf59a2f2
chey22 <cheyennemangum@gmail.com>
2020-02-21 15:12:29 -0600

    commit2

--- reacting-app/src/components/app.jsx
+++ reacting-app/src/components/app.jsx
@@ -5,16 +5,18 @@ class App extends Component {
     super();
 
     this.state = {
-      text: 'this!',
-      inputText: 'type something',
+      text: "this stuff!",
+      inputText: "type something",
       hasLoaded: false
     };
   }
 
+  //i
   componentDidMount() {
-      this.setState({ hasLoaded: true })
+    this.setState({ hasLoaded: true });
   }
 
+  //text entered by user in input field
   handleOnChange(e) {
     this.setState({ inputText: e.target.value });
     // console.log(e);
@@ -22,9 +24,10 @@ class App extends Component {
     // console.log(e.target.value);
   }
 
+  //button clicked
   handleOnClick() {
     this.setState({ hasLoaded: !this.state.hasLoaded });
-    console.log(this.state.hasLoaded);
+    // console.log(this.state.hasLoaded);
   }
 
   render() {
@@ -34,7 +37,11 @@ class App extends Component {
           <h1>
             {this.props.message} {this.state.text}
           </h1>
+          <h3>
+            {this.props.h2message}
+          </h3>
           <input
+            type="text"
             placeholder={this.state.inputText}
             onChange={e => this.handleOnChange(e)}
           />
--- reacting-app/src/index.js
+++ reacting-app/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom'
 
 import App from './components/app'
 
-ReactDOM.render(<App message="I'm learning"/>, document.getElementById('root'))
+ReactDOM.render(<App message="I'm learning" h2message="WOOHOO!"/>, document.getElementById('root'))
